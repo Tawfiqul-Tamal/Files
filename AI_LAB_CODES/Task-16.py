6b72b803d1a67e73f71618de727d7c1d00706848
Md. Tawfiqul Islam Tamal <146660961+Tawfiqul-Tamal@users.noreply.github.com>
2026-02-26 13:26:14 +0600

    AI Lab Task Mid

---
+++ AI_LAB_CODES/Task-16.py
@@ -0,0 +1,8 @@
+# Program to add two numbers entered by the user
+
+x = float(input("Enter 1st number: "))
+y = float(input("Enter 2nd number: "))
+
+z = x + y
+
+print("Sum of", x, "and", y, "is", z)
