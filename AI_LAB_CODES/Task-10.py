8c1b6371c904996b0e43936e2ea1654495ef16cc
Md. Tawfiqul Islam Tamal <146660961+Tawfiqul-Tamal@users.noreply.github.com>
2026-02-26 13:05:28 +0600

    AI Lab Task Mid

---
+++ AI_LAB_CODES/Task-10.py
@@ -0,0 +1,13 @@
+# Program to check if a number is prime
+
+number = int(input("Enter a number: "))
+
+if number <= 1:
+    print("The number is not prime")
+else:
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            print("The number is not prime")
+            break
+    else:
+        print("The number is prime")
