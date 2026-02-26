fb0b70f0d5ccfb753c0c1d139350c880e160590a
Md. Tawfiqul Islam Tamal <146660961+Tawfiqul-Tamal@users.noreply.github.com>
2026-02-26 13:02:05 +0600

    AI Lab Task Mid

---
+++ AI_LAB_CODES/Task-7.py
@@ -0,0 +1,9 @@
+# Program to check if two numbers are equal
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if num1 == num2:
+    print("Both numbers are Equal")
+else:
+    print("The numbers are Not Equal")
